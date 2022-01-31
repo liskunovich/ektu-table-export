d73227af3bc3300049b8bc853310f8eb97bcb925
David <david.leonov.96@mail.ru>
2022-01-31 19:20:08 +0600

    first commit

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import requests
 
 URL = "https://www.do.ektu.kz/PReports/Schedule/ScheduleGroup.asp?page=3&GroupID=12072"
 response = requests.get("https://www.do.ektu.kz/PReports/Schedule/ScheduleGroup.asp?page=3&GroupID=12072")
-print(response.text)
+# print(response.text)
 
