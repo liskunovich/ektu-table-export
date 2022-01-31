628779c4b40587cb9723cc03d3301e8c86255d79
David <david.leonov.96@mail.ru>
2022-01-31 20:58:56 +0600

    31.01 lab work

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-import bs4
+from bs4 import BeautifulSoup
 import time
 import requests
-
+import urllib3
 
 URL = "https://www.do.ektu.kz/PReports/Schedule/ScheduleGroup.asp?page=3&GroupID=12072"
-response = requests.get("https://www.do.ektu.kz/PReports/Schedule/ScheduleGroup.asp?page=3&GroupID=12072")
-# print(response.text)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+    'accept': '*/*'}
 
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=headers, params=params)
+    r.encoding = 'utf8'
+    return r
+
+
+def get_content(html):
+    data = []
+    soup = BeautifulSoup(html, 'html.parser')
+    group_name = soup.find_all(text=re.compile('\[(.*\d?)\]'))
+    table = soup.find_all('table', attrs={'class': 'lineItemsTable'})
+    # print(html)
+    print(group_name)
+
+
+def parse():
+    html = get_html(URL)
+    if html.status_code == 200:
+        print("Site is working")
+        get_content(html.text)
+    else:
+        print("Site is not working")
+
+
+parse()
